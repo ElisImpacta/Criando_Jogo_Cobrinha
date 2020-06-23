188862aaa26609dfe83c1faa508f6047d71954ca
ElisImpacta <pessoaelisrosa@gmail.com>
2020-06-23 17:08:02 +0100

    Corrigindo o background

--- js/snake_game.js
+++
@@ -1,9 +0,0 @@
-let canvas = document.getElementById("fundo");
-let context = canvas.getcontext("2d");
-let box = 32;
-
-function criandoBG(){
-    context.fillstyle = "green";
-    context.fillRect(0, 0, 16 * box, 16 * box);
-}
-criandoBG();
