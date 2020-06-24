18a13fcf7906cf01ce5218647edfce721710002b
ElisImpacta <pessoaelisrosa@gmail.com>
2020-06-24 18:53:48 +0100

    Quando a cobrinha comer uma comida, ela cresce e a aparece outra comida em outro lugar

--- snake_game.js
+++ snake_game.js
@@ -9,6 +9,7 @@ snake[0] = {
 }
 //Iniciando a direção da cobrinha para a direita
 let direction = "right";
+//Iniciando o lugar da comidinha
 let food = {
     x: Math.floor(Math.random() * 15 + 1) * box,
     y: Math.floor(Math.random() * 15 + 1) * box
@@ -32,8 +33,8 @@ function criandoSnake() {
 
 //Desenhando a comida e fazendo elas aparecerem
 function criandoFood(){
-    context.fillStyle = "pink"
-    context.fillRect(food.x, food.x, box, box);
+    context.fillStyle = "blue"
+    context.fillRect(food.x, food.y, box, box);
 }
 
 //Chamando os botões do teclado
@@ -49,7 +50,7 @@ function tecla(event){
 
 //Função de inicializar e atualizar o jogo
 function iniciandoGame(){
-//Se a cobrinha passar da parede, aparecerá do outro lado, o mesmo ocorre cima e baixo;
+    //Se a cobrinha passar da parede, aparecerá do outro lado, o mesmo ocorre cima e baixo;
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "up") snake[0].y = 0;
@@ -62,12 +63,19 @@ function iniciandoGame(){
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
+    //Definindo as direções da cobrinha
     if(direction == "right") snakeX += box;
     if(direction == "left") snakeX -= box;
     if(direction == "up") snakeY += box;
     if(direction == "down") snakeY -= box;
 
-    snake.pop();
+    //Quando a cobrinha comer uma comida, ela cresce e a aparece outra comida em outro lugar
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }else{
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
 
     let novaCabeca = {
         x: snakeX,
