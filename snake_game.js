2909ef296d05008946ccfe875c2fc11b9c6e894d
ElisImpacta <pessoaelisrosa@gmail.com>
2020-06-24 17:42:06 +0100

    Fazendo a cobrinha se mexer

--- snake_game.js
+++ snake_game.js
@@ -1,5 +1,5 @@
 ////////////////Variáveis utilizadas
-let canvas = document.getElementById("snake");
+let canvas = document.getElementById("quadro");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
@@ -7,10 +7,11 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
 
 //////////////Funções utilizadas
 
-//Criando o corpo do joguinho
+//Criando o corpo do joguinho / quadro
 function criandoBG(){
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
@@ -24,5 +25,29 @@ function criandoSnake() {
         
     }
 }
-criandoBG();
-criandoSnake();
+
+//Função de inicializar e atualizar o jogo
+function iniciandoGame(){
+    criandoBG();
+    criandoSnake();
+    
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY += box;
+    if(direction == "down") snakeY -= box;
+
+    snake.pop();
+
+    let novaCabeca = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(novaCabeca);
+
+}
+
+let jogo = setInterval(iniciandoGame, 100);
